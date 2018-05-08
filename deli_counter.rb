e3ecf761acea3561c5b032f4f537c13cb18f612b
david landau <thelandaus@gmail.com>
2018-05-08 14:15:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,8 @@ def line(deli_line)
   if deli_line === 0
     puts "The line is currently empty."
   else
-    
+    deal_line.each_with_index do |customer|
+      puts "The line is currently:" (index + 1).to_s 
   end
 end
 
